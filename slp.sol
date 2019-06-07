a8847751e4d6ff1c78a34064d2ab9be621b3e62c
SilkPayVIP <51476343+SilkPayVIP@users.noreply.github.com>
2019-06-07 17:02:54 +0800

    Create slp.sol

---
+++ slp.sol
@@ -0,0 +1,72 @@
+```pragma solidity ^0.4.25;
+
+contract SafeMath {
+    constructor() public {
+    }
+    
+    function safeAdd(uint256 _x, uint256 _y) internal pure returns (uint256) {
+        uint256 z = _x + _y;
+        assert(z >= _x);
+        return z;
+    }
+    
+    function safeSub(uint256 _x, uint256 _y) internal pure returns (uint256) {
+        assert(_x >= _y);
+        return _x - _y;
+    }
+}
+
+/**
+    Silubium SRC20 Token Standard Token implementation
+*/
+contract SLPToken is SafeMath {
+    uint8 public constant decimals = 8;
+
+    string public constant name = 'SilkPay';//2019.6.4 running
+    string public constant symbol = 'SLP';
+    uint256 public totalSupply = 2100 * 10000 * 10**uint256(decimals);
+
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+    constructor() public {
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    modifier validAddress(address _address) {
+        require(_address != 0x0);
+        _;
+    }
+
+    function transfer(address _to, uint256 _value) public validAddress(_to) returns (bool success)
+    {
+        balanceOf[msg.sender] = safeSub(balanceOf[msg.sender], _value);
+        balanceOf[_to] = safeAdd(balanceOf[_to], _value);
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public validAddress(_from) validAddress(_to) returns (bool success)
+    {
+        allowance[_from][msg.sender] = safeSub(allowance[_from][msg.sender], _value);
+        balanceOf[_from] = safeSub(balanceOf[_from], _value);
+        balanceOf[_to] = safeAdd(balanceOf[_to], _value);
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public validAddress(_spender) returns (bool success)
+    {
+        require(_value == 0 || allowance[msg.sender][_spender] == 0);
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function () public payable {
+        revert();
+    }
+}```
